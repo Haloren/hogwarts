9d01d063e76f1027a9a7f5d06c32538a0d5ab997
Haloren <mckennakyler@gmail.com>
2020-11-22 22:09:05 -0700

    created containers folder and HogsContainer

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+// import HelloWorld from "./HelloWorld";
+import HogsContainer from '../containers/HogsContainer';
 
 class App extends Component {
+  
+  
+  
   render() {
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        {/* <HelloWorld /> */}
+        <HogsContainer />
       </div>
     );
   }
---
+++ src/containers/HogsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HogsContainer (props) {
+    console.log(props)
+
+    return(
+        <div>
+            <h1>Hogs Container</h1>
+        </div>
+    )
+}
